8734ece8fe9c7a2ef110711c89f6fd0e00d707e1
20Federico <fefe.molino@gmail.com>
2021-11-29 23:12:12 +0100

    snack 3 completed

--- snack3.php
+++ snack3.php
@@ -1,1 +1,28 @@
 <!-- Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta -->
+
+<?php
+  $randomNumbers = [];
+
+  while (count($randomNumbers) < 15) {
+    $randomNumber = rand(0, 100);
+    if (!in_array($randomNumber, $randomNumbers)) {
+      $randomNumbers[] = $randomNumber;
+    }
+  }
+  
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+  <pre>
+    <?php var_dump($randomNumbers) ?>
+  </pre>
+</body>
+</html>
