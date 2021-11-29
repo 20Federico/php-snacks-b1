5d59d7964d8c67d207a2de341c07079bf93b3b72
20Federico <fefe.molino@gmail.com>
2021-11-29 16:45:56 +0100

    init

---
+++ snack1.php
@@ -0,0 +1,7 @@
+<!-- Passare come parametri GET name, mail e age
+verificare (cercando i metodi che non conosciamo nella documentazione) 
+che name sia più lungo di 3 caratteri, 
+che mail contenga un punto e una chiocciola 
+che age sia un numero. 
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” -->
+
